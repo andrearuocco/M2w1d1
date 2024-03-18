f6c46919ad3ed7bc629b2f7e11484bc38070dc21
andrearuocco <ruoca96@gmail.com>
2024-03-18 17:29:20 +0100

    questionjs

---
+++ prove.js
@@ -0,0 +1,19 @@
+/*
+
+1) Un algoritmo può essere visto come un insieme di istruzioni che consentono di arrivare ad un
+determinato obiettivo partendo da alcuni dati iniziali. Anche una ricetta o un insieme di cose 
+da fare per ottenere un risultato sono "esempi comuni" di algoritmi, la differenza in informatica
+sta nel fatto che i passaggi per arrivare al piatto pronto o le cose da fare vengono espresse attraverso 
+un linguaggio di programmazione. 
+
+2) Una variabile va considerata come un contenitore pronto per essere riempito. Quando creo una variabile
+sto preparando uno spazio per determinate informazioni che in seguito verranno definite, quest'ultime però
+sono successive e per esistere hanno bisogno di uno spazio che le ospiti. Questo spazio è dato 
+dall'esistenza della variabile (di una porzione di memoria in informatica).
+
+3) Il tipo undefined identifica la variabile come non ancora pronta per contenere qualcosa e si distingue
+dal tipo null che invece consente di ripulire il contenuto di una variabile già esistente. Si può ottenere
+un errore di tipo undefined quando andiamo per esempio a richiamare una variabile che non abbiamo 
+dichiarato in precedneza.
+
+*/
